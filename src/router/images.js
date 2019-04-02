b8df9c28b8be216f947992f73f61ae1a4071f7ad
datmai <dat.mai@hdwebsoft.co>
2019-04-02 22:17:17 +0700

    Code UI form chat

---
+++ src/router/images.js
@@ -0,0 +1,179 @@
+import SearchIcons from '@/assets/images/search.png'
+import Chat from '@/assets/images/chat.png'
+import Location from '@/assets/images/location.png'
+import DigitalCampaign from '@/assets/images/digital-campaign.png'
+import TeamWork from '@/assets/images/teamwork.png'
+
+// flag
+import flag1 from '@/assets/images/svg/flag1.svg'
+import flag2 from '@/assets/images/svg/flag2.svg'
+import flag3 from '@/assets/images/svg/flag3.svg'
+import flag4 from '@/assets/images/svg/flag4.svg'
+import flag5 from '@/assets/images/svg/flag5.svg'
+import flag6 from '@/assets/images/svg/flag6.svg'
+import flag7 from '@/assets/images/svg/flag7.svg'
+import flag8 from '@/assets/images/svg/flag8.svg'
+import flag9 from '@/assets/images/svg/flag9.svg'
+import flag10 from '@/assets/images/svg/flag10.svg'
+import flag11 from '@/assets/images/svg/flag11.svg'
+import flag12 from '@/assets/images/svg/flag12.svg'
+import flag13 from '@/assets/images/svg/flag13.svg'
+import flag14 from '@/assets/images/svg/flag14.svg'
+import flag15 from '@/assets/images/svg/flag15.svg'
+import flag16 from '@/assets/images/svg/flag16.svg'
+import flag17 from '@/assets/images/svg/flag17.svg'
+import flag18 from '@/assets/images/svg/flag18.svg'
+import flag19 from '@/assets/images/svg/flag19.svg'
+import flag20 from '@/assets/images/svg/flag20.svg'
+import flag21 from '@/assets/images/svg/flag21.svg'
+import flag22 from '@/assets/images/svg/flag22.svg'
+import flag23 from '@/assets/images/svg/flag23.svg'
+import flag24 from '@/assets/images/svg/flag24.svg'
+import flag25 from '@/assets/images/svg/flag25.svg'
+import flag26 from '@/assets/images/svg/flag26.svg'
+import flag27 from '@/assets/images/svg/flag27.svg'
+import flag28 from '@/assets/images/svg/flag28.svg'
+import flag29 from '@/assets/images/svg/flag29.svg'
+import flag30 from '@/assets/images/svg/flag30.svg'
+import flag31 from '@/assets/images/svg/flag31.svg'
+import flag32 from '@/assets/images/svg/flag32.svg'
+import flag33 from '@/assets/images/svg/flag33.svg'
+import flag34 from '@/assets/images/svg/flag34.svg'
+import flag35 from '@/assets/images/svg/flag35.svg'
+import flag36 from '@/assets/images/svg/flag36.svg'
+import flag37 from '@/assets/images/svg/flag37.svg'
+import flag38 from '@/assets/images/svg/flag38.svg'
+import flag39 from '@/assets/images/svg/flag39.svg'
+import flag40 from '@/assets/images/svg/flag40.svg'
+import flag41 from '@/assets/images/svg/flag41.svg'
+import flag42 from '@/assets/images/svg/flag42.svg'
+import flag43 from '@/assets/images/svg/flag43.svg'
+import flag44 from '@/assets/images/svg/flag44.svg'
+import flag45 from '@/assets/images/svg/flag45.svg'
+import flag46 from '@/assets/images/svg/flag46.svg'
+import flag47 from '@/assets/images/svg/flag47.svg'
+import flag48 from '@/assets/images/svg/flag48.svg'
+import flag49 from '@/assets/images/svg/flag49.svg'
+import flag50 from '@/assets/images/svg/flag50.svg'
+import flag51 from '@/assets/images/svg/flag51.svg'
+import flag52 from '@/assets/images/svg/flag52.svg'
+import flag53 from '@/assets/images/svg/flag53.svg'
+import flag54 from '@/assets/images/svg/flag54.svg'
+
+// emoji
+import emoji1 from '@/assets/images/svg/emoji1.svg'
+import emoji2 from '@/assets/images/svg/emoji2.svg'
+import emoji3 from '@/assets/images/svg/emoji3.svg'
+import emoji4 from '@/assets/images/svg/emoji4.svg'
+import emoji5 from '@/assets/images/svg/emoji5.svg'
+import emoji6 from '@/assets/images/svg/emoji6.svg'
+import emoji7 from '@/assets/images/svg/emoji7.svg'
+import emoji8 from '@/assets/images/svg/emoji8.svg'
+import emoji9 from '@/assets/images/svg/emoji9.svg'
+import emoji10 from '@/assets/images/svg/emoji10.svg'
+import emoji11 from '@/assets/images/svg/emoji11.svg'
+import emoji12 from '@/assets/images/svg/emoji12.svg'
+import emoji13 from '@/assets/images/svg/emoji13.svg'
+import emoji14 from '@/assets/images/svg/emoji14.svg'
+import emoji15 from '@/assets/images/svg/emoji15.svg'
+import emoji16 from '@/assets/images/svg/emoji16.svg'
+import emoji17 from '@/assets/images/svg/emoji17.svg'
+import emoji18 from '@/assets/images/svg/emoji18.svg'
+import emoji19 from '@/assets/images/svg/emoji19.svg'
+import emoji20 from '@/assets/images/svg/emoji20.svg'
+import emoji21 from '@/assets/images/svg/emoji21.svg'
+import emoji22 from '@/assets/images/svg/emoji22.svg'
+import emoji23 from '@/assets/images/svg/emoji23.svg'
+
+const imgs = {
+  icons: {
+    SearchIcons,
+    Chat,
+    Location,
+    DigitalCampaign,
+    TeamWork
+  },
+  flags: {
+    flag1,
+    flag2,
+    flag3,
+    flag4,
+    flag5,
+    flag6,
+    flag7,
+    flag8,
+    flag9,
+    flag10,
+    flag11,
+    flag12,
+    flag13,
+    flag14,
+    flag15,
+    flag16,
+    flag17,
+    flag18,
+    flag19,
+    flag20,
+    flag21,
+    flag22,
+    flag23,
+    flag24,
+    flag25,
+    flag26,
+    flag27,
+    flag28,
+    flag29,
+    flag30,
+    flag31,
+    flag32,
+    flag33,
+    flag34,
+    flag35,
+    flag36,
+    flag37,
+    flag38,
+    flag39,
+    flag40,
+    flag41,
+    flag42,
+    flag43,
+    flag44,
+    flag45,
+    flag46,
+    flag47,
+    flag48,
+    flag49,
+    flag50,
+    flag51,
+    flag52,
+    flag53,
+    flag54
+  },
+  emoji: {
+    emoji1,
+    emoji2,
+    emoji3,
+    emoji4,
+    emoji5,
+    emoji6,
+    emoji7,
+    emoji8,
+    emoji9,
+    emoji10,
+    emoji11,
+    emoji12,
+    emoji13,
+    emoji14,
+    emoji15,
+    emoji16,
+    emoji17,
+    emoji18,
+    emoji19,
+    emoji20,
+    emoji21,
+    emoji22,
+    emoji23
+  }
+}
+
+export default imgs
